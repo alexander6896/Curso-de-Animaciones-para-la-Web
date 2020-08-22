4649de0fedac542e484da4848c74a18e4f68a43e
Alexander Vladimir Muñiz Escobedo <alexander6896@hotmail.com>
2020-08-22 18:29:49 -0500

    Controlar animacion con botones y eventos de JS

--- javascript-animate/javascript/main.js
+++ javascript-animate/javascript/main.js
@@ -1,7 +1,7 @@
 const $pelota = document.getElementById('pelota');
 
 /* element.animate(keyframes = [], option ={}) */
-$pelota.animate([
+const animation = $pelota.animate([
     /* from */
     {
         transform: 'translateX(0)'
@@ -15,8 +15,26 @@ $pelota.animate([
     delay: 1000,
     direction: 'normal',
     easing: 'linear',
-    interations: Infinity,
+    iterations: Infinity,
     fill: 'forwards',
     iterationStart: .5, // =50%
     /* endDelay:5000, */
+})
+
+const $playButton = document.getElementById('play');
+const $pauseButton = document.getElementById('pause');
+const $stopButton = document.getElementById('stop');
+const $reverseButton = document.getElementById('reverse');
+
+$playButton.addEventListener('click', (event) => {
+    animation.play();
+})
+$pauseButton.addEventListener('click', (event) => {
+    animation.pause();
+})
+$stopButton.addEventListener('click', (event) => {
+    animation.cancel();
+})
+$reverseButton.addEventListener('click', (event) => {
+    animation.reverse();
 })
